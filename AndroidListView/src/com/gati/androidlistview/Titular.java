d748b37be35bc63d67fc3bddcae2c40898908d4c
Noe Cubillas <noecubi@gmail.com>
2014-01-28 13:34:15 +0100

    Android List View v1.0

---
+++ AndroidListView/src/com/gati/androidlistview/Titular.java
@@ -0,0 +1,19 @@
+package com.gati.androidlistview;
+
+public class Titular {
+	private String titulo;
+	private String subtitulo;
+
+	public Titular(String tit, String sub){
+		titulo = tit;
+		subtitulo = sub;
+	}
+	
+	public String getTitulo(){
+		return titulo;
+	}
+	
+	public String getSubtitulo(){
+		return subtitulo;
+	}
+}
